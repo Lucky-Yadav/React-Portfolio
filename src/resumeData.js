f782e501a277595735853c320bf8dd0b231ffcf7
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-07-24 10:21:08 +0530

    data change

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,112 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Lucky Yadav",
+  role: "Full Stack Web-Developer",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/lucky-yadav-b523a2221/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/LuckyYadav786",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "I am currently a final year student at The Indiara Gandhi University and pursuing my B.sc.Computer.Science from here. I am a Full Stack Web Developer, currently diving deeper into React.js. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "India",
+  website: "https://rbhatia46.github.io",
+  education: [
+    {
+      UniversityName: "The Indiara Gandhi University",
+      specialization: "B.Sc.Computer.Science",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2022",
+      // Achievements: "Some Achievements",
+    },
+    // {
+    //   UniversityName: "Some University",
+    //   specialization: "Some specialization",
+    //   MonthOfPassing: "Jan",
+    //   YearOfPassing: "2018",
+    //   Achievements: "Some Achievements",
+    // },
+  ],
+  work: [
+    {
+      CompanyName: "Fresher",
+      // specialization: "Some specialization",
+      // MonthOfLeaving: "Jan",
+      // YearOfLeaving: "2018",
+      // Achievements: "Some Achievements",
+    },
+    // {
+    //   CompanyName: "Some Company",
+    //   specialization: "Some specialization",
+    //   MonthOfLeaving: "Jan",
+    //   YearOfLeaving: "2018",
+    //   Achievements: "Some Achievements",
+    // },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+  //   {
+  //     skillname: "Reactjs",
+  //   },
+  //   {
+  //     skillname: "MongoDB",
+  //   },
+  //   {
+  //     skillname: "Javascript",
+  //   },
+  //   {
+  //     skillname: "Github",
+  //   },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "Apollo247.com clone",
+      imgurl: "images/portfolio/Apollo.png",
+    },
+    {
+      name: "project2",
+      description: "Acko.com clone",
+      imgurl: "images/portfolio/Acko.png",
+    },
+    {
+      name: "project3",
+      description: "Unsplash.com clone",
+      imgurl: "images/portfolio/Unsplash.png",
+    },
+    // {
+    //   name: "project4",
+    //   description: "mobileapp",
+    //   imgurl: "images/portfolio/phone.jpg",
+    // },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
   
   export default resumeData
